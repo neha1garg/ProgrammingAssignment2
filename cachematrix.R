49bc4113cf89bf643596ef8c3a4c4d552d795a65
neha1garg <neha1garg@users.noreply.github.com>
2015-05-24 09:56:40 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+cache<-NULL
+set_matrix<-function(new_value){
+x<<-new_value
+cache<-NULL
+}
+get_matrix<-function(){
+x
+}
+cache_inverse<-fuction(solve){
+cache<-solve
+}
 
 }
 
